6f654399d61849dae45afcc47f7c3a7267292920
Šárka Princová Maroušková <sarka.marouskova@gmail.com>
2021-05-01 11:47:37 +0200

    Zprovoznění navigace na úzkých displejích

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './style.css';
+/*V JavaScriptu vyberte ze stránky ikonku #nav-btn. Připojte k ní posluchač události click. Tento posluchač prvku nav odebere nebo přidá třídu nav-closed. Klikáním na ikonku tak můžeme zobrazovat nebo skrývat navigaci.
+Navigaci budeme chtít schovat i po přesunutí na nějakou sekci. Připojte tedy posluchač události také na všechny položky navigace. Zařiďte, aby se navigace při kliknutí na libovolnou její položku schovala. Zde se vám jistě bude hodit metoda querySelectorAll.
+Jakmile je váš kód funkční, proveďte commmit s hezky popisnou zprávou a pushněte do vzdáleného repozitáře.*/
 
-console.log('funguju!');
+const navigationBtn = document.querySelector('.nav-btn');
+const navigationElm = document.querySelector('nav');
+navigationBtn.addEventListener('click', () => {
+  navigationElm.classList.toggle('nav-closed');
+});
+
+document.querySelectorAll('.nav-link').forEach((item) => {
+  item.addEventListener('click', () => {
+    navigationElm.classList.add('nav-closed');
+  });
+});
